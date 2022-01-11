189918b70aac7240c90b3045928038c3c31ed4c3
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-01-11 10:54:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// const arr = () => {
+//     console.log("Jamshed")
+// }
+// arr()
+
+
+// const arr = () =>  "Jamshed"
+// console.log(arr())
+
+
+
+
+// const arr = (a, b) => {
+//     console.log(a + b)
+// }
+// arr(+prompt("enter value"),+prompt("enter value 2"))
+
+
+
+// const arr = a => {
+//     alert(a)
+// }
+// arr(+prompt("enter value"))
+
+
+
+
+const arr = (a, b) => 
+a * b
+
+document.getElementById("demo").innerHTML = arr(4, 6)
+
+
+
+
+
